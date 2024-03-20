9e3ee606ea42978ed1cc2391f6d2f0889ac18103
PRAGY112508 <153983693+PRAGY112508@users.noreply.github.com>
2024-03-20 14:48:22 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="stream",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple example package for streaming services",
+    author="Praganesh",
+    author_email="praganeshsubramanian2@gmail.com",
+    # url="https://github.com/PRAGY112508/stream",
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
